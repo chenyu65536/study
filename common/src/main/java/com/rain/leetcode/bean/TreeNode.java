0381898951015f8a6fbc4b24db9959c15d9785df
chenyu65536 <chenyu65536@163.com>
2023-09-20 16:31:15 +0800

    leetcode

--- common/src/main/java/com/rain/leetcode/bean/TreeNode.java
+++ common/src/main/java/com/rain/leetcode/bean/TreeNode.java
@@ -18,6 +18,21 @@ public class TreeNode {
         this.right = right;
     }
 
+    public static TreeNode createTree(Integer[] array, int index) {
+        TreeNode treeNode = null;
+        if (index < array.length) {
+            Integer value = array[index];
+            if (value == null) {
+                return null;
+            }
+            treeNode = new TreeNode(value);
+            treeNode.left = createTree(array, 2 * index + 1);
+            treeNode.right = createTree(array, 2 * index + 2);
+            return treeNode;
+        }
+        return treeNode;
+    }
+
     @Override
     public String toString() {
         return "[" + val + "]";
